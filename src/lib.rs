7fbd48cfd9464a9532a0ac1daeefd0943fec5d14
Thomas Epperson <thomas.epperson@snapon.com>
2024-05-31 14:10:43 -0500

    Initial commit.

---
+++ src/lib.rs
@@ -0,0 +1,55 @@
+pub struct MutexGuard<'a, T> {
+    mutex: &'a Mutex<T>,
+    inner: parking_lot::MutexGuard<'a, T>,
+}
+
+impl<'a, T> std::ops::Deref for MutexGuard<'a, T> {
+    type Target = parking_lot::MutexGuard<'a, T>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.inner
+    }
+}
+
+impl<'a, T> std::ops::DerefMut for MutexGuard<'a, T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.inner
+    }
+}
+
+impl<'a, T> Drop for MutexGuard<'a, T> {
+    fn drop(&mut self) {
+        self.mutex.drop_obtained();
+    }
+}
+
+pub struct Mutex<T> {
+    inner: parking_lot::Mutex<T>,
+    obtained: parking_lot::Mutex<Option<std::backtrace::Backtrace>>,
+}
+
+impl<T> Mutex<T> {
+    pub fn new(t: T) -> Self {
+        Self {
+            inner: parking_lot::Mutex::new(t),
+            obtained: parking_lot::Mutex::new(None),
+        }
+    }
+
+    pub fn lock(&self) -> MutexGuard<'_, T> {
+        MutexGuard {
+            mutex: &self,
+            inner: self.inner.lock(),
+        }
+    }
+
+    pub fn is_obtained(&self) -> &parking_lot::Mutex<Option<std::backtrace::Backtrace>> {
+        &self.obtained
+    }
+
+    fn drop_obtained(&self) {
+        let mut a = self.obtained.lock();
+        a.take();
+    }
+}
+
